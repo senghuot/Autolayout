057d7ea06af97be222809d295b7c79f4fd2e71af
senghuot <senghuotlim@gmail.com>
2016-04-05 16:06:31 -0700

    half working

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +25,10 @@ class ViewController: UIViewController {
         secure = !secure
     }
     
+    var loggedInUser: User? { didSet { updateUI() } }
+    
     @IBAction func login(sender: UIButton) {
-        
+        loggedInUser = User.login(loginField.text!, password: passwordField.text!)
     }
     
     var secure = false { didSet { updateUI() } }
@@ -33,6 +36,17 @@ class ViewController: UIViewController {
     private func updateUI() {
         passwordField.secureTextEntry = secure
         passwordLabel.text = secure ? "Secure Password" : "Password"
+        imageView.image = loggedInUser?.image
     }
 }
 
+private extension User
+{
+    var image: UIImage? {
+        if let image = UIImage(named: login) {
+            return image
+        } else {
+            return UIImage(named: "unknown_user")
+        }
+    }
+}
